8fd5b7f0b27804c0f9e90c22af5269b9ab2a1950
devmartsuriname <info@devmart.sr>
2025-09-27 23:59:21 +0530

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { HelmetProvider } from "react-helmet-async"
+import AppNavigation from "./navigation/Navigation"
+import { Provider } from 'react-redux'
+import store from "./redux/store"
 
-const queryClient = new QueryClient();
+function App() {
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+  return (
+    <>
+      <Provider store={store}>
+        <HelmetProvider>
+          <AppNavigation />
+        </HelmetProvider>
+      </Provider>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/index.css'
+import '../public/assets/scss/main.scss'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
